9eaed516fb6b1e446b939d726a21d840288d48cf
pisc639-NT <peerapong.pisc@newton.ac.th>
2025-01-22 18:53:57 +0700

    move h1 style js to css

--- themes/dark/script.js
+++ themes/dark/script.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('div.row.mt-100>div>*').forEach(el=>el&&el.remove());
 document.querySelectorAll('body > div.container-fluid > div > div.col.d-none.d-md-block > div.row.header-nav.pd-10 > div.col-4.d-flex.justify-content-start.align-items-center > a > img').forEach(el=>el&&el.remove());
-document.querySelectorAll('div.row.mt-100>div').forEach(e=>e.insertAdjacentHTML('afterbegin','<h1 style="color:#fff;">VocabSize</h1>'));
+document.querySelectorAll('div.row.mt-100>div').forEach(e=>e.insertAdjacentHTML('afterbegin','<h1>VocabSize</h1>'));
 (el=>el&&el.remove())(document.querySelector('body>div>div:nth-child(3)>div>div>div:nth-child(3)'));
 (el=>el&&el.remove())(document.querySelector('body>div>div:nth-child(3)>div>div>div:nth-child(2)'));
 document.querySelectorAll(".txt_input").forEach(function(e){let event=new Event('change');let autofillValue=e.value;e.value="";e.dispatchEvent(event);e.value=autofillValue;e.dispatchEvent(event);});
