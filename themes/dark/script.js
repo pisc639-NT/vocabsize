b00f40229be3b4c5e0b935137ef2be67c92a9988
pisc639-NT <peerapong.pisc@newton.ac.th>
2025-01-22 18:56:19 +0700

    make it f-string

--- themes/dark/script.js
+++ themes/dark/script.js
@@ -4,4 +4,4 @@ document.querySelectorAll('div.row.mt-100>div').forEach(e=>e.insertAdjacentHTML(
 (el=>el&&el.remove())(document.querySelector('body>div>div:nth-child(3)>div>div>div:nth-child(3)'));
 (el=>el&&el.remove())(document.querySelector('body>div>div:nth-child(3)>div>div>div:nth-child(2)'));
 document.querySelectorAll(".txt_input").forEach(function(e){let event=new Event('change');let autofillValue=e.value;e.value="";e.dispatchEvent(event);e.value=autofillValue;e.dispatchEvent(event);});
-fetch('https://raw.githubusercontent.com/pisc639-NT/vocabsize/refs/heads/main/themes/dark/style.css').then(x=>x.text()).then(x=>document.head.insertAdjacentHTML('beforeend', '<style>'+x+'</style>">'));
+fetch('https://raw.githubusercontent.com/pisc639-NT/vocabsize/refs/heads/main/themes/dark/style.css').then(x=>x.text()).then(x=>document.head.insertAdjacentHTML('beforeend',`<style>${x}</style>">`));
