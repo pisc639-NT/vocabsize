0c39bfb2948f2837287252a85443a8f5afef4504
pisc639-NT <peerapong.pisc@newton.ac.th>
2025-01-24 18:33:17 +0700

    Added cursor.svg, improve queryselector, move images inside themes folder, change old cursor to cursor-old.svg

--- themes/dark/script2.js
+++ themes/dark/script2.js
@@ -1,20 +1,33 @@
 let d=document;
 let ds=(e=>d.querySelector(e));
 let dsa=(e=>d.querySelectorAll(e));
-let dsah=(e=>dsa(e).forEach(e=>e.style.display="None"));
+let dsaHide=(e=>dsa(e).forEach(e=>e.style.display="None"));
+let dsaText=((e,t)=>dsa(e).forEach(e=>e.innerText=t));
+let dsaAdd=((e,p,h)=>dsa(e).forEach(e=>e.insertAdjacentHTML(p,h)));
+let dsaAttr=((e,k,v)=>dsa(e).forEach(e=>e.setAttribute(k,v)));
+
 
 // dsa('div.row.mt-100>div>*').forEach(e=>e&&e.remove());
-dsah('div.row.mt-100>div>*')
-dsa('div.row.mt-100>div').forEach(e=>e.insertAdjacentHTML('afterbegin','<h1>VocabSize</h1>'));
-
+dsaHide('div.row.mt-100>div>*')
+// dsa('div.row.mt-100>div').forEach(e=>e.insertAdjacentHTML('afterbegin','<h1>VocabSize</h1>'));
+dsaAdd('div.row.mt-100>div','afterbegin','<h1>VocabSize</h1>');
 // dsa("div.col-4>a>img").forEach(e=>e&&e.remove());
 // dsa("div.col-4>a").forEach(e=>e.innerText="VocabSize");
-dsa("[src='https://vocabsize.xeersoft.co.th/assets/images/logo_on_bar.png']").forEach(function(e){e.parentElement.innerText="VocabSize";e.remove();});
-dsa("[for='txt_email']").forEach(e=>e.innerText="\xa0\xa0\xa0\xa0Username");
-dsa("[for='txt_password']").forEach(e=>e.innerText="\xa0\xa0\xa0\xa0Password");
+// dsa("[for='txt_email']").forEach(e=>e.innerText="\xa0\xa0\xa0\xa0Username");
+// dsa("[for='txt_password']").forEach(e=>e.innerText="\xa0\xa0\xa0\xa0Password");
+dsaText("[for='txt_email']","\xa0\xa0\xa0\xa0Username");
+dsaText("[for='txt_password']","\xa0\xa0\xa0\xa0Password");
+
+// $$("#mySidenav,#sideNavMobile")
+dsaAttr("#mySidenav,#sideNavMobile","style","width: 300px")
+
+dsaHide("[src='https://vocabsize.xeersoft.co.th/assets/images/logo_on_bar.png']")
+// dsa("[src='https://vocabsize.xeersoft.co.th/assets/images/logo_on_bar.png']")
+dsaText("a:has(>[src='https://vocabsize.xeersoft.co.th/assets/images/logo_on_bar.png'])","VocabSize")
+
 
 // dsa("body>div>div:nth-child(3)>div>div>div:nth-child(n+2)").forEach((e=>e&&e.remove()));
-dsah("body>div>div:nth-child(3)>div>div>div:nth-child(n+2)")
+dsaHide("body>div>div:nth-child(3)>div>div>div:nth-child(n+2)")
 
 dsa(".txt_input").forEach(function(e){let event=new Event('change');let autofillValue=e.value;e.value="";e.dispatchEvent(event);e.value=autofillValue;e.dispatchEvent(event);});
 dsa(".btn_listen_word1, .btn_stop_word1").forEach(e=>e.classList.remove("span_hidden"));
