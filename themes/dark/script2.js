c198efbedd0aff28eb49be8907ae68d89d02cef0
pisc639-NT <peerapong.pisc@newton.ac.th>
2025-01-24 19:46:09 +0700

    Query Selector Shorter, etc.

--- themes/dark/script2.js
+++ themes/dark/script2.js
@@ -1,12 +1,12 @@
 let d=document;
-let ds=(e=>d.querySelector(e));
-let dsa=(e=>d.querySelectorAll(e));
-let dsaEach=((e,l)=>d.querySelectorAll(e).forEach(l));
-let dsaHide=(e=>dsa(e).forEach(e=>e.style.display="None"));
-let dsaText=((e,t)=>dsa(e).forEach(e=>e.innerText=t));
-let dsaAdd=((e,p,h)=>dsa(e).forEach(e=>e.insertAdjacentHTML(p,h)));
-let dsaAttr=((e,k,v)=>dsa(e).forEach(e=>e.setAttribute(k,v)));
-
+let ds=(q=>d.querySelector(q));
+let dsa=(q=>d.querySelectorAll(q));
+let dsaEach=((q,l)=>dsa(q).forEach(l));
+let dsaHide=(q=>dsaEach(q,e=>e.style.display="None"));
+let dsaText=((q,t)=>dsaEach(q,e=>e.innerText=t));
+let dsaAdd=((q,p,h)=>dsaEach(q,e=>e.insertAdjacentHTML(p,h)));
+let dsaAttr=((q,k,v)=>dsaEach(q,e=>e.setAttribute(k,v)));
+let dsaUnClick=(q=>dsaEach(q,e=>getEventListeners(e).click.forEach(l=>e.removeEventListener("click",l.listener))));
 
 // dsa('div.row.mt-100>div>*').forEach(e=>e&&e.remove());
 dsaHide('div.row.mt-100>div>*')
@@ -20,7 +20,7 @@ dsaText("[for='txt_email']","\xa0\xa0\xa0\xa0Username");
 dsaText("[for='txt_password']","\xa0\xa0\xa0\xa0Password");
 
 // $$("#mySidenav,#sideNavMobile")
-dsaAttr("#mySidenav,#sideNavMobile","style","width: 300px")
+// dsaAttr("#mySidenav,#sideNavMobile","style","width:300px")
 
 dsaHide("[src$='logo_on_bar.png']")
 // dsa("[src='https://vocabsize.xeersoft.co.th/assets/images/logo_on_bar.png']")
@@ -28,11 +28,15 @@ dsaText("a:has(>[src$='logo_on_bar.png'])","VocabSize")
 
 
 // dsa("body>div>div:nth-child(3)>div>div>div:nth-child(n+2)").forEach((e=>e&&e.remove()));
-dsaHide("body>div>div:nth-child(3)>div>div>div:nth-child(n+2)")
+// dsaHide("body>div>div:nth-child(3)>div>div>div:nth-child(n+2)")
+dsaHide("[href='https://vocabsize-legacy.xeersoft.co.th']")
 
-dsa(".txt_input").forEach(function(e){let event=new Event('change');let autofillValue=e.value;e.value="";e.dispatchEvent(event);e.value=autofillValue;e.dispatchEvent(event);});
-dsaEach(".btn_listen_word1, .btn_stop_word1",e=>e.classList.remove("span_hidden"));
+dsaEach(".txt_input",function(e){let event=new Event('change');let autofillValue=e.value;e.value="";e.dispatchEvent(event);e.value=autofillValue;e.dispatchEvent(event);});
+dsaEach(".btn_listen_word1,.btn_stop_word1",e=>e.classList.remove("span_hidden"));
 
-$$("img[src$='btn_speech.png']").forEach(function(e){e.src="https://raw.githubusercontent.com/pisc639-NT/vocabsize/refs/heads/main/themes/images/icon/Settings.svg";e.width="30"})
+
+dsaUnClick(".memu-img")
+dsaEach(".menu-img",e=>e.addEventListener(p=>dsaAttr("#mySidenav,#sideNavMobile","style","width:300px")))
+dsaEach("img[src$='btn_speech.png']",function(e){e.src="https://raw.githubusercontent.com/pisc639-NT/vocabsize/refs/heads/main/themes/images/icon/Settings.svg";e.width="30"})
 
 fetch('https://raw.githubusercontent.com/pisc639-NT/vocabsize/refs/heads/main/themes/dark/style.css').then(x=>x.text()).then(x=>d.head.insertAdjacentHTML('beforeend',`<style>${x}</style>">`));
