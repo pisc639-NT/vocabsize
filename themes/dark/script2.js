15b271b33344df0d94895c0e8384caeb5d736546
pisc639-NT <peerapong.pisc@newton.ac.th>
2025-01-24 18:45:29 +0700

    Query Selector from = to $=, Fix the Wrong setting image path from before updated moved folder problem

--- themes/dark/script2.js
+++ themes/dark/script2.js
@@ -1,6 +1,7 @@
 let d=document;
 let ds=(e=>d.querySelector(e));
 let dsa=(e=>d.querySelectorAll(e));
+let dsaEach=((e,l)=>d.querySelectorAll(e).forEach(l));
 let dsaHide=(e=>dsa(e).forEach(e=>e.style.display="None"));
 let dsaText=((e,t)=>dsa(e).forEach(e=>e.innerText=t));
 let dsaAdd=((e,p,h)=>dsa(e).forEach(e=>e.insertAdjacentHTML(p,h)));
@@ -21,17 +22,17 @@ dsaText("[for='txt_password']","\xa0\xa0\xa0\xa0Password");
 // $$("#mySidenav,#sideNavMobile")
 dsaAttr("#mySidenav,#sideNavMobile","style","width: 300px")
 
-dsaHide("[src='https://vocabsize.xeersoft.co.th/assets/images/logo_on_bar.png']")
+dsaHide("[src$='logo_on_bar.png']")
 // dsa("[src='https://vocabsize.xeersoft.co.th/assets/images/logo_on_bar.png']")
-dsaText("a:has(>[src='https://vocabsize.xeersoft.co.th/assets/images/logo_on_bar.png'])","VocabSize")
+dsaText("a:has(>[src$='logo_on_bar.png'])","VocabSize")
 
 
 // dsa("body>div>div:nth-child(3)>div>div>div:nth-child(n+2)").forEach((e=>e&&e.remove()));
 dsaHide("body>div>div:nth-child(3)>div>div>div:nth-child(n+2)")
 
 dsa(".txt_input").forEach(function(e){let event=new Event('change');let autofillValue=e.value;e.value="";e.dispatchEvent(event);e.value=autofillValue;e.dispatchEvent(event);});
-dsa(".btn_listen_word1, .btn_stop_word1").forEach(e=>e.classList.remove("span_hidden"));
+dsaEach(".btn_listen_word1, .btn_stop_word1",e=>e.classList.remove("span_hidden"));
 
-$$("img[src='https://vocabsize.xeersoft.co.th/assets/images/btn_speech.png']").forEach(function(e){e.src="https://raw.githubusercontent.com/pisc639-NT/vocabsize/refs/heads/main/images/icon/Settings.svg";e.width="30"})
+$$("img[src$='btn_speech.png']").forEach(function(e){e.src="https://raw.githubusercontent.com/pisc639-NT/vocabsize/refs/heads/main/themes/images/icon/Settings.svg";e.width="30"})
 
 fetch('https://raw.githubusercontent.com/pisc639-NT/vocabsize/refs/heads/main/themes/dark/style.css').then(x=>x.text()).then(x=>d.head.insertAdjacentHTML('beforeend',`<style>${x}</style>">`));
